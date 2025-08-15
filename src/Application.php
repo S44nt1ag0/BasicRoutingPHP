d0a332db1b65bf07927419d057641222ed595c06
s44nt1ag0 <santiago.develop1@gmail.com>
2025-08-15 19:43:53 -0300

    Add Application class to initialize and start the router with defined routes

---
+++ src/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SimpleApi;
+
+class Application
+{
+  public function start() {
+    $router = new Router();
+
+    $router->create("GET", "/", function () {
+      http_response_code(200);
+      return;
+    });    
+
+    $router->create("GET", "/hello", function () {
+      http_response_code(response_code: 200);
+      echo json_encode(value: ["hello" => "world"]);
+      return;
+    });
+
+    $router->init();
+  }
+}
