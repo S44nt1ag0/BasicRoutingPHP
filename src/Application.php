6b4a303028d237651e8214e7424aad85e4a1d384
s44nt1ag0 <santiago.develop1@gmail.com>
2025-08-16 13:34:44 -0300

    Refactor Application class to enhance user creation endpoint and improve response handling

--- src/Application.php
+++ src/Application.php
@@ -1,20 +1,55 @@
 <?php
 
 namespace SimpleApi;
+use SimpleApi\Database\Connection;
 
 class Application
 {
-  public function start() {
+  public function start()
+  {
     $router = new Router();
 
     $router->create("GET", "/", function () {
-      http_response_code(200);
-      return;
-    });    
-
-    $router->create("GET", "/hello", function () {
       http_response_code(response_code: 200);
       echo json_encode(value: ["hello" => "world"]);
+      return;
+    });
+
+    $router->create("POST", "/user", function () {
+      $pdo = Connection::getConnection();
+
+      $input = json_decode(file_get_contents("php://input"), true);
+
+      if (
+        !isset($input['user']) ||
+        !isset($input['email']) ||
+        !isset($input['name']) ||
+        !isset($input['password'])
+      ) {
+        http_response_code(400);
+        echo json_encode(["error" => "Required fields: user, email, name, password"]);
+        return;
+      }
+
+      $stmt = $pdo->prepare("INSERT INTO users (user, email, name, password) VALUES (:user, :email, :name, :password)");
+
+      try {
+
+        $stmt->execute([
+          ':user' => $input['user'],
+          ':email' => $input['email'],
+          ':name' => $input['name'],
+          ':password' => password_hash($input['password'], PASSWORD_BCRYPT)
+        ]);
+
+      } catch (\PDOException $e) {
+        http_response_code(500);
+        echo json_encode(["error" => "erro to create user."]);
+        return;
+      }
+
+      http_response_code(response_code: 201);
+      echo json_encode(value: ["message" => "User created successfully"]);
       return;
     });
 
