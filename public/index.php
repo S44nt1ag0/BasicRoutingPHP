cdf88b925d16423c935459c95f21418d51c745be
s44nt1ag0 <santiago.develop1@gmail.com>
2025-08-24 11:48:31 -0300

    Refactor index.php to improve schema synchronization handling and code organization

--- public/index.php
+++ public/index.php
@@ -1,8 +1,17 @@
 <?php
 
-use SimpleApi\Application;
-
 include __DIR__ . '/../vendor/autoload.php';
+
+use SimpleApi\Application;
+use SimpleApi\Database\SchemaSync;
+
+try {
+    SchemaSync::sync();
+} catch (Exception $e) {
+    error_log('Schema sync failed: ' . $e->getMessage());
+    http_response_code(500);
+    exit('Database schema synchronization error.');
+}
 
 $app = new Application();
 
